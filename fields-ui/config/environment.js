95fd1c208d268a536d9d49a561135b2923e6dbe3
Adam Donahue <adam@peeriq.com>
2016-12-11 18:27:14 -0500

    Change name.

--- fields-ui/config/environment.js
+++ fields-ui/config/environment.js
@@ -2,7 +2,7 @@
 
 module.exports = function(environment) {
   var ENV = {
-    modulePrefix: 'x',
+    modulePrefix: 'fields-ui',
     environment: environment,
     rootURL: '/',
     locationType: 'auto',
