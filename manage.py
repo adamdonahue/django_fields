bd6faa0ff113d3f10b9974c460c40249a6030c9f
Adam Donahue <adam@peeriq.com>
2016-12-11 19:15:00 -0500

    Updates.

--- manage.py
+++ manage.py
@@ -1,7 +1,38 @@
 #!/usr/bin/env python
+
 import os
+import re
 import sys
 
+
+def initialize_env(env_file):
+    """
+    Initialize the environment with variable settings from
+    a .env file in the current directory.  These settings
+    override any existing settings and can be leveraged
+    within Django's settings.py file.
+    """
+    try:
+        with open(env_file) as f:
+            entries = [line.strip() for line in f.readlines()]
+    except IOError as e:
+        entries = []
+
+    for entry in entries:
+        if not entry or re.match(r'\s*#', entry):
+            continue
+        k, v = entry.split('=', 1)
+        if not re.match(r'[A-Za-z_]+$', k):
+            raise RuntimeError("Invalid environment variable name: %s" % k)
+
+        # If the value is quoted, remove the quotations.
+        if v.startswith("'") and v.endswith("'") \
+            or v.startswith('"') and v.endswith('"'):
+                v = v[1:-1]
+            
+        # Note that already-set environment variables take precedence.
+        os.environ.setdefault(k, v)
+    
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django_fields.settings")
     try:
@@ -19,4 +50,9 @@ if __name__ == "__main__":
                 "forget to activate a virtual environment?"
             )
         raise
+
+    # Initialize any custom environment variables stores in a
+    # .env file (or a file pointed to by DJANGO_ENV).
+    initialize_env(os.environ.get("DJANGO_ENV", ".env"))
+
     execute_from_command_line(sys.argv)
